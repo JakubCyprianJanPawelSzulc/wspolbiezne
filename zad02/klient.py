47c9a87cb43ecb533e0490a8c7a216c4001fc17c
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-10-16 15:18:34 +0200

    zad02 fix

--- zad02/klient.py
+++ zad02/klient.py
@@ -3,23 +3,35 @@ import time
 
 def client():
     serverLockFile = "serverLockFile.lock"
+    clientFile = input("Podaj nazwę swojego pliku: ")
     while True:
+        if not os.path.exists(clientFile):
+            with open(clientFile, "w") as file:
+                pass
+        with open(clientFile, "r") as clientInput:
+            if os.stat(clientFile).st_size == 0:
+                pass
+            else:
+                print("Odpowiedź serwera:")
+                for line in clientInput:
+                    print(line, end="")
+                break
         if os.path.exists(serverLockFile):
             print("Serwer zajęty")
             time.sleep(3)
             continue
-        with open (serverLockFile, "w") as file:
+        with open(serverLockFile, "w") as file:
             file.write("lock")
-        
-        serverInputFile="serverInputFile.txt"
 
-        with open(serverInputFile, "r") as serverInput:
-            for line in serverInput:
-                print(line, end="")
-        response = input("Podaj odpowiedź dla serwera (Esc aby zakończyć): ")
-        if response == "Esc":
-            break
-            
+        serverInputFile = "serverInputFile.txt"
+        with open(serverInputFile, "w") as file:
+            file.write(clientFile + "\n")
+            while True:
+                line = input("Podaj linię do pliku dla serwera (esc aby zakończyć): ")
+                if line == "esc":
+                    break
+                file.write(line + "\n")
+
 
 if __name__ == "__main__":
     client()
--- zad02/serwer.py
+++ zad02/serwer.py
@@ -3,29 +3,27 @@ import time
 
 def main():
     while True:
-        lockfile="serverLockFile.lock"
+        lockfile = "serverLockFile.lock"
         if os.path.exists(lockfile):
+            clientFile = ""
+            with open("serverInputFile.txt", "r") as file:
+                clientFile = file.readline().strip()
+            if not clientFile:
+                continue
+            with open("serverInputFile.txt", "r") as serverInputFile:
+                print("Tekst klienta:")
+                for line in serverInputFile:
+                    print(line, end="")
+                with open(clientFile, "w") as clientFile:
+                    while True:
+                        line = input("Podaj linię do pliku dla klienta (esc aby zakończyć): ")
+                        if line == "esc":
+                            break
+                        clientFile.write(line + "\n")
+            os.remove(lockfile)
+        else:
             time.sleep(1)
             continue
-        with open(lockfile, "w") as file:
-            file.write("lock")
-        
-        with open("serverInputFile.txt", "w") as inputFile:
-            clientFile = input("podaj nazwę pliku klienta: ")
-            while True:
-                line = input ("Podaj linię do pliku dla klienta (Esc aby zakończyć): ")
-                if line == "Esc":
-                    break
-                inputFile.write(line+"\n")
-        os.remove(lockfile)
-
-        with open(clientFile, "r") as clientInput:
-            print("Tekst klienta")
-            for line in clientFile:
-                print(line, end="")
-
-        with open (lockfile, "w") as file:
-            file.write("unlocked")
 
 if __name__ == "__main__":
     main()
