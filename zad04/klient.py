3b0a9d25484bf36e24353ef469e70660fc1ced02
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-11-03 22:06:41 +0100

    changes in zad04

--- zad04/klient.py
+++ zad04/klient.py
@@ -1,8 +1,8 @@
 import win32pipe
 import win32file
 
-def client():
-    client_queue = win32file.CreateFile(
+def client(id):
+    server_queue = win32file.CreateFile(
         r'\\.\pipe\server_queue',
         win32file.GENERIC_READ | win32file.GENERIC_WRITE,
         0,
@@ -12,21 +12,39 @@ def client():
         None
     )
 
+    my_queue = win32pipe.CreateNamedPipe(
+        r'\\.\pipe\client_queue_' + id,
+        win32pipe.PIPE_ACCESS_DUPLEX,
+        win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+        1, 65536, 65536,
+        0,
+        None
+    )
+
     win32pipe.SetNamedPipeHandleState(
-        client_queue,
+        server_queue,
         win32pipe.PIPE_READMODE_MESSAGE,
         None,
         None
     )
 
+    requested_id = input("Podaj poszukiwane ID (lub 'exit' aby zakończyć): ")
+    message = id + ':' + requested_id
+    win32file.WriteFile(server_queue, message.encode())
+
     while True:
-        requested_id = input("Podaj ID: ")
-        win32file.WriteFile(client_queue, requested_id.encode())
-        response = win32file.ReadFile(client_queue, 65536)[1].decode()
-        print(f"Odpowiedź: {response}")
+        try:
+            win32pipe.ConnectNamedPipe(my_queue, None)
+            response = win32file.ReadFile(my_queue, 65536)[1].decode()
+            print(f"Odpowiedź: {response}")
+            break
+        except:
+            pass
+        
 
 if __name__ == "__main__":
     try:
-        client()
+        id = input("podaj swoje ID:")
+        client(id)
     except KeyboardInterrupt:
         print("Klient zostaje zatrzymany...")
--- zad04/serwer.py
+++ zad04/serwer.py
@@ -1,6 +1,8 @@
 import win32pipe
 import win32file
 import os
+import time
+import pywintypes
 
 database = {
     1: "Podolski",
@@ -19,33 +21,55 @@ def lookup_id(requested_id):
         for line in f:
             if line.startswith(str(requested_id)):
                 return line.split(':')[1].strip()
-
     return "Nie ma"
 
-def server():
-    if not os.path.exists('database.txt'):
-        create_database()
-
-    server_queue = win32pipe.CreateNamedPipe(
-        r'\\.\pipe\server_queue',
-        win32pipe.PIPE_ACCESS_DUPLEX,
-        win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
-        1, 65536, 65536,
-        0,
-        None
-    )
-
-    win32pipe.ConnectNamedPipe(server_queue, None)
-
+def handle_client(client_queue):
     while True:
-        requested_id = win32file.ReadFile(server_queue, 65536)[1].decode()
+        requested_id = win32file.ReadFile(client_queue, 65536)[1].decode()
         print(f"Otrzymano zapytanie o ID: {requested_id}")
-
+        time.sleep(3)
         response = lookup_id(int(requested_id))
         print(f"Odpowiedź: {response}")
+        win32file.WriteFile(client_queue, response.encode())
 
-        win32file.WriteFile(server_queue, response.encode())
+def server():
 
+    while True:
+        try:
+            server_queue = win32pipe.CreateNamedPipe(
+                r'\\.\pipe\server_queue',
+                win32pipe.PIPE_ACCESS_DUPLEX,
+                win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+                1, 65536, 65536,
+                0,
+                None
+            )
+            win32pipe.ConnectNamedPipe(server_queue, None)
+            message = win32file.ReadFile(server_queue, 65536)[1].decode()
+            # ... (tutaj obsługujesz wiadomość)
+            client_id, requested_id = message.split(':')
+            print(f"Otrzymano zapytanie o ID: {requested_id}")
+            time.sleep(3)
+            response = lookup_id(int(requested_id))
+            print(f"Odpowiedź: {response}")
+            client_response_queue = win32file.CreateFile(
+                r'\\.\pipe\client_queue_' + client_id,
+                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+                0,
+                None,
+                win32file.OPEN_EXISTING,
+                0,
+                None
+            )
+            win32file.WriteFile(client_response_queue, response.encode())
+
+        except pywintypes.error as e:
+            if e.args[0] == 109:
+                print("Klient zakończył połączenie")
+            else:
+                raise
+        finally:
+            win32file.CloseHandle(server_queue)
 
 if __name__ == "__main__":
     try:
