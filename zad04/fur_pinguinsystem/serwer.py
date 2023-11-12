07b6ba1f348ed94eeb09d6e2d76457dcf32808a5
Jakub <j.szulc.328@studms.ug.edu.pl>
2023-11-12 14:24:33 +0100

    zad04 signals

--- zad04/fur_pinguinsystem/serwer.py
+++ zad04/fur_pinguinsystem/serwer.py
@@ -3,9 +3,11 @@ import sys
 import json
 import time
 import threading
+import signal
 
 server_queue_path = "/tmp/server_queue"
 database = {1: "Podolski", 2: "Klose", 3: "Mueller", 4: "Ballack"}
+message_lock = threading.Lock()
 
 def handle_client_request(client_queue_path, client_id, requested_ID):
     time.sleep(3)
@@ -16,25 +18,50 @@ def handle_client_request(client_queue_path, client_id, requested_ID):
             response = "Nie ma"
 
         with open(client_queue_path, 'w') as client_queue:
-            client_queue.write(response)
+            with message_lock:
+                client_queue.write(response)
 
     except Exception as e:
         print(f"Obsługa klienta {client_id} nie powiodła się: {e}")
 
-if not os.path.exists(server_queue_path):
-    os.mkfifo(server_queue_path)
+def handle_signals(signum,frame):
+    if signum == signal.SIGTERM or signum == signal.SIGHUP:
+        print(f"otrzymano sygnał {signum}")
+    elif signum == signal.SIGUSR1:
+        print("otrzymano sygnał SIGUSR1. Koniec.")
 
 def main():
+    signal.signal(signal.SIGTERM, handle_signals)
+    signal.signal(signal.SIGHUP, handle_signals)
+    signal.signal(signal.SIGUSR1, handle_signals)
+
+    if not os.path.exists(server_queue_path):
+        os.mkfifo(server_queue_path)
+
     while True:
         with open(server_queue_path, 'r') as server_queue:
-            request = json.loads(server_queue.read())
+            data = server_queue.read().strip()
 
-        client_queue_path = request['client_queue_path']
-        client_id = request['client_id']
-        requested_id = request['ID']
+            try:
+                request = json.loads(data)
 
-        thread = threading.Thread(target=handle_client_request, args=(client_queue_path, client_id, requested_id))
-        thread.start()
+                if isinstance(request, dict):
+                    client_queue_path = request['client_queue_path']
+                    client_id = request['client_id']
+                    requested_id = request['ID']
 
+                    thread = threading.Thread(target=handle_client_request, args=(client_queue_path, client_id, requested_id))
+                    thread.start()
+                else:
+                    print(f"Otrzymano sygnał: {data}")
+                    if data == "10":
+                        sys.exit(0)
+                    if data == "1":
+                        print("Otrzymano sygnał SIGHUP")
+                    elif data == "15":
+                        print("Otrzymano sygnał SIGTERM")
+
+            except json.JSONDecodeError:
+                print(f"Otrzymano sygnał: {data}")
 if __name__ == "__main__":
     main()
