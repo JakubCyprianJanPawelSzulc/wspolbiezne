d97598d61cc0725e9fa2ce2165e84084c9f06cfb
Jakub <j.szulc.328@studms.ug.edu.pl>
2024-01-05 11:53:02 +0100

    zad11 added ui

--- zad11/klient.py
+++ zad11/klient.py
@@ -1,31 +1,65 @@
+import tkinter as tk
+from tkinter import messagebox
+import threading
+import time
 import socket
 
-def connect_to_server(port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', port))
-    print('Connected to server on port', port)
-    return client_socket
+class CrosswordClient:
+    def __init__(self,root):
+        self.root = root
+        self.create_ui()
+        self.client_socket=self.create_communication_thread(12345)
 
-def close_connection(socket):
-    socket.close()
-    print('Connection closed')
+    def create_ui(self):
+        self.entry_grid = [[None] * 10 for _ in range(10)]
+        for i in range(10):
+            for j in range(10):
+                entry = tk.Entry(self.root, width=2, justify="center", state=tk.DISABLED)
+                entry.grid(row=i, column=j)
+                self.entry_grid[i][j] = entry
+        self.root.bind('<Key>', self.handle_key)
 
-def receive_data(socket):
-    data = socket.recv(1024).decode()
-    return data
+    
+    def create_communication_thread(self, port):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', port))
+        print('Connected to server on port', port)
+        return client_socket
 
-def send_data(socket, data):
-    socket.send(data.encode())
+    def wait_for_response(self):
+        server_address = ('localhost', 12345)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect(server_address)
+            while True:
+                response = self.receive_data(client_socket)
+                if response is not None:
+                    self.update_crossword(response)
+
+    def receive_data(self, socket):
+        data = socket.recv(1024).decode()
+        return data if data else None
+    
+    def update_crossword(self, response):
+        for i, row in enumerate(response):
+            for j, char in enumerate(row):
+                self.entry_grid[i][j].delete(0, tk.END)
+                self.entry_grid[i][j].insert(0, char)
+                self.entry_grid[i][j].config(state=tk.DISABLED)
+
+    def handle_key(self, event):
+        data = event.char.upper()
+        self.send_data(data)
+        print("Sent data:", data)
+
+    def send_data(self, data):
+        self.client_socket.sendall(data.encode())
+
 
 if __name__ == '__main__':
-    try:
-        client_socket = connect_to_server(12345)
-        while True:
-            message = input('Enter message: ')
-            send_data(client_socket, message)
-            response = receive_data(client_socket)
-            print('Received response:', response)
+    root = tk.Tk()
+    root.title("Crossword Client")
 
-    except KeyboardInterrupt:
-        close_connection(client_socket)
-        
+    client = CrosswordClient(root)
+
+    root.mainloop()
--- zad11/serwer.py
+++ zad11/serwer.py
@@ -1,35 +1,58 @@
 import socket
+import threading
+import time
 
-def start_server(port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', port))
-    server_socket.listen()
-    print('Server is listening on port', port)
-    client_socket, client_address = server_socket.accept()
-    print('Client connected from', client_address)
-    return client_socket
+class CrosswordGame:
+    def __init__(self, words):
+        self.words = words
+        self.current_word_index = 0
+        self.current_word = self.words[self.current_word_index]
+        self.lock = threading.Lock()
 
-def close_server(socket):
-    socket.close()
-    print('Server closed')
+        self.board = [[''] * len(self.current_word) for _ in range(len(self.words))]
 
-def receive_data(socket):
-    data = socket.recv(1024).decode()
-    return data
+    def start_server(self, port):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('localhost', port))
+        server_socket.listen()
+        print('Server is listening on port', port)
 
-def send_data(socket, data):
-    socket.send(data.encode())
+        client_socket, client_address = server_socket.accept()
+        print('Client connected from', client_address)
+
+        self.wait_for_response(client_socket)
+
+    def wait_for_response(self, client_socket):
+        while True:
+            time.sleep(1)
+            response = self.get_response_from_player(client_socket)
+            if response is not None:
+                self.update_crossword(response)
+                if self.is_word_completed():
+                    print("Word completed!")
+                    self.current_word_index += 1
+                    if self.current_word_index < len(self.words):
+                        self.current_word = self.words[self.current_word_index]
+
+    def get_response_from_player(self, client_socket):
+        with self.lock:
+            response = client_socket.recv(1024).decode()
+            print("Received data:", response)
+            return response if response else None
+
+    def update_crossword(self, response):
+        with self.lock:
+            for i, char in enumerate(response):
+                self.board[self.current_word_index][i] = char
+
+    def is_word_completed(self):
+        with self.lock:
+            return '' not in self.board[self.current_word_index]
 
 if __name__ == '__main__':
     der_port = 12345
-    server_socket = start_server(der_port)
 
-    try:
-        while True:
-            received_data = receive_data(server_socket)
-            print('Received data:', received_data)
-            response=input('Enter response: ')
-            send_data(server_socket, response)
+    words = ["PYTHON", "JAVA", "ANDRZEJ", "JACEK", "METANABOL", "DIANABOL", "STERYDY", "BMW", "ZAZA", "CRACK"]
 
-    except KeyboardInterrupt:
-        close_server(server_socket)
+    game = CrosswordGame(words)
+    game.start_server(der_port)
