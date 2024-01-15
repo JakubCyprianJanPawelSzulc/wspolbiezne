4be32a08cf3fe1a71c07ffdd44ea136ee419f224
Jakub <j.szulc.328@studms.ug.edu.pl>
2024-01-15 13:24:39 +0100

    zad10 fix

--- zad10/pierwszePlus.py
+++ zad10/pierwszePlus.py
@@ -1,73 +1,60 @@
-import time
 import math
-from multiprocessing import Pool, Manager
-
-l = 1000000
-r = 2000000
-
-def pierwsza(k):
- for i in range (2,k-1):
-   if i*i>k:
-     return True
-   if k%i == 0:
-     return False
- return True
+import time
+from multiprocessing import Pool
 
 
-def pierwsza1(k,mlp):
- for p in mlp:
-   if k%p == 0:
-     return False
-   if p*p>k:
-     return True
- return True
+def czy_pierwsza(k):
+    for i in range(2, int(math.sqrt(k)) + 1):
+        if k % i == 0:
+            return False
+    return True
 
 
-def licz(l,r):
-  mlp = []
-  s = math.ceil(math.sqrt(r))
-  for i in range (2,s+1):
-    if pierwsza(i):
-       mlp.append(i)
-  pierwsze = []
-  for i in range (l,r+1):
-    if pierwsza1(i,mlp):
-       pierwsze.append(i)
-    return pierwsze
-  
-def blizniacze_sekwencyjnie(l,r):
-  pierwsze = []
-  for i in range (l,r+1):
-    if pierwsza(i) and pierwsza(i+2):
-       pierwsze.append((i,i+2))
-  return pierwsze
+def stworz_mlp(r):
+    mlp = []
+    s = math.ceil(math.sqrt(r))
+    for i in range(2, s + 1):
+        if czy_pierwsza(i):
+            mlp.append(i)
+    return mlp
 
-def blizniacze_rownolegle_pomocnicza(chunk, shared_list):
-    result = blizniacze_sekwencyjnie(chunk[0], chunk[1])
-    shared_list.extend(result)
 
-def blizniacze_rownolegle(l, r):
-    with Manager() as manager:
-        shared_list = manager.list()
-        pool = Pool()
-        chunks = [(i, i + 10000) for i in range(l, r + 1, 10000)]
-        pool.starmap(blizniacze_rownolegle_pomocnicza, [(chunk, shared_list) for chunk in chunks])
-        pool.close()
-        pool.join()
-        return list(shared_list)
+def znajdz_pierwsze(l, r, mlp):
+    primes = set()
+    s = math.ceil(math.sqrt(r))
+    for i in range(l, r + 1):
+        for p in mlp:
+            if i % p == 0:
+                break
+            if p * p > i:
+                primes.add(i)
+                break
+        primes.add(i)
+    return primes
 
-def main():
-    start = time.time()
-    wynik1 = blizniacze_sekwencyjnie(l,r)
-    end = time.time()
-    print("Czas sekwencyjnie: ",end - start)
-    start = time.time()
-    wynik2 = blizniacze_rownolegle(l,r)
-    end = time.time()
-    print("Czas rownolegle: ",end - start)
-    # print("porównanie wyników: ", wynik1 == wynik2)
-    # print("wynik1: ", len(wynik1))
-    # print("wynik2: ", len(wynik2))
 
 if __name__ == '__main__':
-    main()
+    left = 1000000
+    right = 2000000
+    mlp = stworz_mlp(right)
+    processes = 4
+
+    print("Start sekwencyjnie")
+    start1 = time.time()
+    solution1 = znajdz_pierwsze(left, right, mlp)
+    end1 = time.time() - start1
+    print("Czas sekwencyjnie: ", end1)
+
+    print("Start równolegle")
+    with Pool(processes) as p:
+        components = []
+        part = (right - left) // processes
+        for i in range(processes):
+            components.append([left + i * part, left + (i + 1) * part, mlp])
+        start2 = time.time()
+        solution2 = p.starmap(znajdz_pierwsze, components)
+        end2 = time.time() - start2
+
+    print("Czas równolegle: ", end2)
+    print("O ile lepsze: ", end1 / end2)
+    print("Czy poprawne rozwiązanie: ", solution1 == set.union(*solution2))
